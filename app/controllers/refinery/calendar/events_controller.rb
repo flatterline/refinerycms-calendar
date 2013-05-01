17afcd93a0ac5c0f6c98356efa4910e0ac37067c
Curtis Miller <curtis@flatterline.com>
2013-05-01 15:00:34 -0400

    Add ics format for event show

--- app/controllers/refinery/calendar/events_controller.rb
+++ app/controllers/refinery/calendar/events_controller.rb
@@ -14,7 +14,10 @@ module Refinery
 
         # you can use meta fields from your model instead (e.g. browser_title)
         # by swapping @page for @event in the line below:
-        present(@page)
+        respond_to do |format|
+          format.html { present(@page) }
+          format.ics
+        end
       end
 
       def archive
