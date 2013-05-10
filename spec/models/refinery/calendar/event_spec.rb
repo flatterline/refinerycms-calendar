93d798b1d8fddd85ed64f030b2b72d82de0ee3cc
Curtis Miller <curtis@flatterline.com>
2013-05-10 14:29:33 -0700

    Update test

--- spec/models/refinery/calendar/event_spec.rb
+++ spec/models/refinery/calendar/event_spec.rb
@@ -3,15 +3,56 @@ require 'spec_helper'
 module Refinery
   module Calendar
     describe Event do
+      before do
+        @event = FactoryGirl.build(:event)
+      end
+
       describe "validations" do
-        subject do
-          FactoryGirl.create(:event,
-          :title => "Refinery CMS")
+
+        describe 'ends_at' do
+
+          it 'is required' do
+            @event.ends_at = nil
+            @event.should_not be_valid
+          end
+
+          it "ensures an event's end is after it's start" do
+            @event.starts_at = "Apr 24, 2013 12:00 pm"
+            @event.ends_at   = "Apr 23, 2013 12:00 pm"
+
+            @event.should_not be_valid
+            @event.errors[:base].should include("An event's end must come after it's start")
+          end
+
         end
 
-        it { should be_valid }
-        its(:errors) { should be_empty }
-        its(:title) { should == "Refinery CMS" }
+        describe 'organizer_id' do
+
+          it 'is required' do
+            @event.organizer_id = nil
+            @event.should_not be_valid
+          end
+
+        end
+
+        describe 'starts_at' do
+
+          it 'is required' do
+            @event.starts_at = nil
+            @event.should_not be_valid
+          end
+
+        end
+
+        describe 'title' do
+
+          it 'is required' do
+            @event.title = ''
+            @event.should_not be_valid
+          end
+
+        end
+
       end
     end
   end
